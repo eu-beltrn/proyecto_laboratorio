5ab963e4999698129222a0f15f986800acc9c2a0
Jonathan Rodriguez <j.rodriguez2018ng@gmail.com>
2026-05-06 18:02:37 -0600

    Agrega entorno y script de ML

---
+++ departamento_MachineLearning/script_prueba_librerias.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sklearn
+import matplotlib
+
+print("=== DEPARTAMENTO DE MACHINE LEARNING ===")
+print(f"Versión de Pandas: {pd.__version__}")
+print(f"Versión de Scikit-Learn: {sklearn.__version__}")
+print(f"Versión de Matplotlib: {matplotlib.__version__}")
+print("¡Todas las librerías cargaron correctamente!")
